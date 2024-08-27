db9523b2e72356a114903dca8408ac5e17e80011
6gh <47262177+6gh@users.noreply.github.com>
2024-08-27 01:06:45 -0400

    lint: fix linting problems

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,43 +1,43 @@
-import antfu from '@antfu/eslint-config';
+import antfu from '@antfu/eslint-config'
 
 export default antfu({
-    ignores: [
-        "**/out",
-        "**/dist",
-        "**/*.d.ts",
-    ],
+  ignores: [
+    '**/out',
+    '**/dist',
+    '**/*.d.ts',
+  ],
 
-    files: [
-        "**/*.ts",
-    ],
+  files: [
+    '**/*.ts',
+  ],
 
-    languageOptions: {
-        ecmaVersion: 5,
-        sourceType: "module",
-    },
+  languageOptions: {
+    ecmaVersion: 5,
+    sourceType: 'module',
+  },
 
-    stylistic: {
-        indent: 2,
-        quotes: "single",
-    },
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+  },
 
-    // Overwrite certain rules to your preference
-    rules: {
-        "no-console": ["error", {
-        allow: ["info", "warn", "trace", "error", "group", "groupEnd"],
-        }],
-        "curly": ["error", "all"],
-        "node/prefer-global/process": ["error", "always"],
-        "eqeqeq": ["error", "always"],
-        "no-throw-literal": "warn",
+  // Overwrite certain rules to your preference
+  rules: {
+    'no-console': ['error', {
+      allow: ['info', 'warn', 'trace', 'error', 'group', 'groupEnd'],
+    }],
+    'curly': ['error', 'all'],
+    'node/prefer-global/process': ['error', 'always'],
+    'eqeqeq': ['error', 'always'],
+    'no-throw-literal': 'warn',
 
-        "style/comma-dangle": ["warn", "always-multiline"],
-        "style/arrow-parens": ["warn", "always"],
-        "style/semi": ["warn", "always"],
-        "style/no-tabs": ["warn", {
-        allowIndentationTabs: false,
-        }],
-        "style/indent": ["warn", 2],
-        "style/quotes": ["warn", "double"],
-    },
+    'style/comma-dangle': ['warn', 'always-multiline'],
+    'style/arrow-parens': ['warn', 'always'],
+    'style/semi': ['warn', 'always'],
+    'style/no-tabs': ['warn', {
+      allowIndentationTabs: false,
+    }],
+    'style/indent': ['warn', 2],
+    'style/quotes': ['warn', 'double'],
+  },
 })
--- src/extension.ts
+++ src/extension.ts
@@ -1,117 +1,115 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as os from 'os';
-import * as chokidar from 'chokidar';
-import * as Color from 'color';
-import template from './template';
+import * as fs from "node:fs";
+import * as path from "node:path";
+import * as os from "node:os";
+import * as vscode from "vscode";
+import * as chokidar from "chokidar";
+import * as Color from "color";
+import template from "./template";
 
-const walCachePath = path.join(os.homedir(), '.cache', 'wal');
-const walColorsPath = path.join(walCachePath, 'colors');
-const walColorsJsonPath = path.join(walCachePath, 'colors.json');
+const walCachePath = path.join(os.homedir(), ".cache", "wal");
+const walColorsPath = path.join(walCachePath, "colors");
+const walColorsJsonPath = path.join(walCachePath, "colors.json");
 let autoUpdateWatcher: chokidar.FSWatcher | null = null;
 
-
 export function activate(context: vscode.ExtensionContext) {
+  // Register the update command
+  const disposable = vscode.commands.registerCommand("walTheme.update", generateColorThemes);
+  context.subscriptions.push(disposable);
 
-	// Register the update command
-	let disposable = vscode.commands.registerCommand('walTheme.update', generateColorThemes);
-	context.subscriptions.push(disposable);
+  // Start the auto update if enabled
+  if (vscode.workspace.getConfiguration().get("walTheme.autoUpdate")) {
+    /*
+     * Update theme at startup
+     * Needed for when wal palette updates while vscode isn't running.
+     * The timeout is required to overcome a limitation of vscode which
+     * breaks the theme auto-update if updated too early at startup.
+     */
+    setTimeout(generateColorThemes, 10000);
 
-	// Start the auto update if enabled
-	if(vscode.workspace.getConfiguration().get('walTheme.autoUpdate')) {
-		/*
-		 * Update theme at startup
-		 * Needed for when wal palette updates while vscode isn't running.
-		 * The timeout is required to overcome a limitation of vscode which
-		 * breaks the theme auto-update if updated too early at startup.
-		 */
-		setTimeout(generateColorThemes, 10000);
+    autoUpdateWatcher = autoUpdate();
+  }
 
-		autoUpdateWatcher = autoUpdate();
-	}
-
-	// Toggle the auto update in real time when changing the extension configuration
-	vscode.workspace.onDidChangeConfiguration(event => {
-		if(event.affectsConfiguration('walTheme.autoUpdate')) {
-			if(vscode.workspace.getConfiguration().get('walTheme.autoUpdate')) {
-				if(autoUpdateWatcher === null) {
-					autoUpdateWatcher = autoUpdate();
-				}
-			}
-			else if(autoUpdateWatcher !== null) {
-				autoUpdateWatcher.close();
-				autoUpdateWatcher = null;
-			}
-		}
-	});
-
+  // Toggle the auto update in real time when changing the extension configuration
+  vscode.workspace.onDidChangeConfiguration((event) => {
+    if (event.affectsConfiguration("walTheme.autoUpdate")) {
+      if (vscode.workspace.getConfiguration().get("walTheme.autoUpdate")) {
+        if (autoUpdateWatcher === null) {
+          autoUpdateWatcher = autoUpdate();
+        }
+      }
+      else if (autoUpdateWatcher !== null) {
+        autoUpdateWatcher.close();
+        autoUpdateWatcher = null;
+      }
+    }
+  });
 }
 
 export function deactivate() {
-
-	// Close the watcher if active
-	if(autoUpdateWatcher !== null) {
-		autoUpdateWatcher.close();
-	}
-
+  // Close the watcher if active
+  if (autoUpdateWatcher !== null) {
+    autoUpdateWatcher.close();
+  }
 }
-
 
 /**
  * Generates the theme from the current color palette and overwrites the last one
  */
 function generateColorThemes() {
-	// Import colors from pywal cache
-	let colors: Color[] | undefined;
-	try {
-		colors = fs.readFileSync(walColorsPath)
-										 .toString()
-										 .split(/\s+/, 16)
-			.map(hex => Color(hex));
-		
-		if (fs.existsSync(walColorsJsonPath)) {
-			type WalJson = {
-				special: {
-					background: string,
-					foreground: string
-				}
-			};
+  // Import colors from pywal cache
+  let colors: Color[] | undefined;
+  try {
+    colors = fs.readFileSync(walColorsPath)
+      .toString()
+      .split(/\s+/, 16)
+      .map((hex) => Color(hex));
 
-			let colorsJson: WalJson;
-			const colorsRaw = fs.readFileSync(walColorsJsonPath).toString();
+    if (fs.existsSync(walColorsJsonPath)) {
+      interface WalJson {
+        special: {
+          background: string
+          foreground: string
+        }
+      }
 
-			try {
-				colorsJson = JSON.parse(colorsRaw);
-			} catch {
-				// The wallpaper path on Windows can cause JSON.parse errors since the
-				// path isn't properly escaped.
-				colorsJson = JSON.parse(colorsRaw
-					.split('\n')
-					.filter((line) => !line.includes('wallpaper'))
-					.join('\n'));
-			}
+      let colorsJson: WalJson;
+      const colorsRaw = fs.readFileSync(walColorsJsonPath).toString();
 
-			colors[0] = Color(colorsJson?.special?.background);
-			colors[7] = Color(colorsJson?.special?.foreground);
-		}
-	} catch(error) {
-		// Not a complete failure if we have colors from the wal colors file, but failed to load from the colors.json
-		if (colors === undefined || colors.length === 0) {
-			vscode.window.showErrorMessage('Couldn\'t load colors from pywal cache, be sure to run pywal before updating.');
-			return;
-		}
+      try {
+        colorsJson = JSON.parse(colorsRaw);
+      }
+      catch {
+        // The wallpaper path on Windows can cause JSON.parse errors since the
+        // path isn't properly escaped.
+        colorsJson = JSON.parse(colorsRaw
+          .split("\n")
+          .filter((line) => !line.includes("wallpaper"))
+          .join("\n"));
+      }
 
-		vscode.window.showWarningMessage('Couldn\'t load all colors from pywal cache');
-	}
-		
-	// Generate the normal theme
-	const colorTheme = template(colors, false);
-	fs.writeFileSync(path.join(__dirname,'..', 'themes', 'wal.json'), JSON.stringify(colorTheme, null, 4));
-	
-	// Generate the bordered theme
-	const colorThemeBordered = template(colors, true);
-	fs.writeFileSync(path.join(__dirname,'..', 'themes', 'wal-bordered.json'), JSON.stringify(colorThemeBordered, null, 4));
+      colors[0] = Color(colorsJson?.special?.background);
+      colors[7] = Color(colorsJson?.special?.foreground);
+    }
+  }
+  catch (error) {
+    console.error(error);
+
+    // Not a complete failure if we have colors from the wal colors file, but failed to load from the colors.json
+    if (colors === undefined || colors.length === 0) {
+      vscode.window.showErrorMessage("Couldn't load colors from pywal cache, be sure to run pywal before updating.");
+      return;
+    }
+
+    vscode.window.showWarningMessage("Couldn't load all colors from pywal cache");
+  }
+
+  // Generate the normal theme
+  const colorTheme = template(colors, false);
+  fs.writeFileSync(path.join(__dirname, "..", "themes", "wal.json"), JSON.stringify(colorTheme, null, 4));
+
+  // Generate the bordered theme
+  const colorThemeBordered = template(colors, true);
+  fs.writeFileSync(path.join(__dirname, "..", "themes", "wal-bordered.json"), JSON.stringify(colorThemeBordered, null, 4));
 }
 
 /**
@@ -119,8 +117,8 @@ function generateColorThemes() {
  * @returns The watcher for the color palette
  */
 function autoUpdate(): chokidar.FSWatcher {
-	// Watch for changes in the color palette of wal
-	return chokidar
-		.watch(walCachePath)
-		.on('change', generateColorThemes);
+  // Watch for changes in the color palette of wal
+  return chokidar
+    .watch(walCachePath)
+    .on("change", generateColorThemes);
 }
